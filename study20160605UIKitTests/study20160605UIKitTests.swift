dce544cea9288725c3f63d8ee0447634bf6a9c1c
mocchicc <mocchicc@me.com>
2016-06-05 17:49:27 +0900

    Initial Commit

---
+++ study20160605UIKitTests/study20160605UIKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  study20160605UIKitTests.swift
+//  study20160605UIKitTests
+//
+//  Created by mocchicc on 2016/06/05.
+//  Copyright © 2016年 mocchicc. All rights reserved.
+//
+
+import XCTest
+@testable import study20160605UIKit
+
+class study20160605UIKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
